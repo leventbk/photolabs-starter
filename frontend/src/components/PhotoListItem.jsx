598143e8855dd4c01034a14cd3048f6eaff1da68
Leventbk <leventbkarakus@hotmail.com>
2023-09-15 16:33:50 -0400

    classNames

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,19 +2,17 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-
-
 const PhotoListItem = (props) => {
   return (
-    <div id={props.key}>
-  
-      <img src={props.imageSource} height="600px"/>
-      <br/>
-      <img src={props.profile} height='150px'/>
-      <p>{props.username}</p> 
-      <p>{props.location.city}-{props.location.country}</p>
-
+    <div className='photo-list__item' key={props.photoId}>
+      <img className='photo-list__image'src={props.imageSource} height="600px"/>
+      <div className="photo-list__general-profile">
+        <img src={props.profile} height='150px' className='photo-list__user-profile'/>
+        <div className="photo-list__details">
+          <p className="photo-list__user-info">{props.username}</p> 
+          <p className="photo-list__user-location">{props.location.city}-{props.location.country}</p>
+        </div>
+      </div>
     </div>
   )
 };

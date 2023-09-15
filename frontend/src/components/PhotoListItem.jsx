8d863ede33e82d0b61885f727fbab67435b6abb1
Leventbk <leventbkarakus@hotmail.com>
2023-09-15 15:42:18 -0400

    data moved to app file and used props

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,27 +3,19 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
+
+const PhotoListItem = (props) => {
   return (
-    <>
-      <img src={sampleDataForPhotoListItem.imageSource} height="600px"/>
+    <div id={props.key}>
+  
+      <img src={props.imageSource} height="600px"/>
       <br/>
-      <img src={sampleDataForPhotoListItem.profile} height='150px'/>
-      <p>{sampleDataForPhotoListItem.username}</p> 
-      <p>{sampleDataForPhotoListItem.location.city}-{sampleDataForPhotoListItem.location.country}</p>
+      <img src={props.profile} height='150px'/>
+      <p>{props.username}</p> 
+      <p>{props.location.city}-{props.location.country}</p>
 
-    </>
+    </div>
   )
 };
 

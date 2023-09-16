fdc71fd98a32ab994c8fa22fe0fb73213a00b01c
Leventbk <leventbkarakus@hotmail.com>
2023-09-16 19:57:26 -0400

    topiclist item added to topiclist and render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import PhotoList from 'components/PhotoList';
+import TopicList from 'components/TopicList';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
+      <TopicList/>
       <PhotoList/>
     </div>
   );
--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,29 +1,37 @@
 import React from "react";
 
 import "../styles/TopicList.scss";
-
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+import TopicListItem from "./TopicListItem";
 
 const TopicList = () => {
+
+  const sampleDataForTopicList = [
+    {
+      id: "1",
+      slug: "topic-1",
+      title: "Nature",
+    },
+    {
+      id: "2",
+      slug: "topic-2",
+      title: "Travel",
+    },
+    {
+      id: "3",
+      slug: "topic-3",
+      title: "People",
+    },
+  ];
+  const topicData = sampleDataForTopicList;
+
   return (
     <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
+      {topicData.map((topicData) => (
+        <TopicListItem 
+          key={topicData.id} 
+          slug={topicData.slug}
+          label={topicData.title}/>
+      ))}
     </div>
   );
 };

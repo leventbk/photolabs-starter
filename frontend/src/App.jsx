873713da7d0c8d84f567c2d5519957a411b63e84
Leventbk <leventbkarakus@hotmail.com>
2023-09-18 18:16:07 -0400

    showModel state condition in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
 import photos from "mocks/photos"
 import topics from "mocks/topics"
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const [showModel, setShowModel] = useState(false);
   return (
     <div className="App">
       <HomeRoute photos={photos} topics={topics}/>
+      {showModel && <PhotoDetailsModal/>}
 
 
 

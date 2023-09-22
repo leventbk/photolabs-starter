b18d44b9d4992282e1285a32767aafa4fd9e99b0
Leventbk <leventbkarakus@hotmail.com>
2023-09-22 16:39:33 -0400

    inModal related photos and profile info with style

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 import PhotoFavButton from 'components/PhotoFavButton';
+import PhotoList from 'components/PhotoList';
+import PhotoListItem from 'components/PhotoListItem';
 
 const PhotoDetailsModal = (props) => {
   const {setShowModal, selectedPhoto, toggleFav, favPhotos} = props;
@@ -23,17 +25,27 @@ const PhotoDetailsModal = (props) => {
       />
 
       <div>
-        <div>
-          <img 
-            className='photo-details-modal__image'
-            src={selectedPhoto.urls.full} 
-          />
-        </div>
-        <div>
-          
+        <img 
+          className='photo-details-modal__image'
+          src={selectedPhoto.urls.full} 
+        />
+        <div className='photo-details-modal__photographer-details'>
+          <img className='photo-details-modal__photographer-profile' src={selectedPhoto.user.profile} />
+          <div className='photo-details-modal__photographer-info' >
+            {selectedPhoto.user.name}
+         
+          <div className='photo-details-modal__photographer-location'>
+            {selectedPhoto.location.city}, {selectedPhoto.location.country}
+          </div>
+          </div>
         </div>
       </div>
-    </div>
+        <div>
+          <h3>Similar Photos</h3>
+          {<PhotoList favPhotos={[]} toggleFav={[]}photos={Object.values(selectedPhoto.similar_photos)}/>}
+        </div>
+      </div>
+
   )
 };
 

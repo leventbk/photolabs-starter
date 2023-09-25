549e73041953491a300890daeec0393af777ce30
Leventbk <leventbkarakus@hotmail.com>
2023-09-25 18:37:21 -0400

    onClick event gor getPhotosByTopic with its Id

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -3,8 +3,12 @@ import React from "react";
 import "../styles/TopicListItem.scss";
 
 const TopicListItem = (props) => {
+
+  const handleOnClick = () => {
+    props.getPhotosbyTopic(props.topicId)
+  }
   return (
-    <div className="topic-list__item" key={props.id}>
+    <div className="topic-list__item" onClick={handleOnClick}>
       <span>{props.label}</span>
     </div>
   );

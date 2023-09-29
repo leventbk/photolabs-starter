2b529f77c829f6223ddbab5677bde6aec008f302
Leventbk <leventbkarakus@hotmail.com>
2023-09-29 14:06:27 -0400

    added comment

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -6,9 +6,10 @@ import '../styles/FavBadge.scss';
 const FavBadge = ({ isFavPhotoExist }) => {
   return (
     <div className='fav-badge'>
-      <FavIcon displayAlert={!!isFavPhotoExist} selected={isFavPhotoExist}/>
+      <FavIcon displayAlert={!!isFavPhotoExist} selected={isFavPhotoExist} />
+      {/* The resulting boolean value from !!isFavPhotoExist is assigned to the prop displayAlert for a React component. This means that the displayAlert prop for the component will be true if isFavPhotoExist is truthy and false if isFavPhotoExist is falsy. */}
     </div>
-  ) 
+  )
 };
 
 export default FavBadge;

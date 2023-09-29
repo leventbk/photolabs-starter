20c41f1fd7bbd9f6f4c21e3ebb8011a89ed205dd
Leventbk <leventbkarakus@hotmail.com>
2023-09-29 13:56:10 -0400

    document format and comments

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,23 +6,26 @@ import PhotoFavButton from "./PhotoFavButton";
 const PhotoListItem = (props) => {
 
   const { photo, setShowModal, setSelectedPhoto, favPhotos, toggleFav } = props;
-  
+
+  // Modal event onClick
   const handleOnClick = () => {
     setSelectedPhoto(photo)
     setShowModal(true)
   }
   return (
     <div className='photo-list__item' key={photo.id}>
-      <PhotoFavButton 
-        photoId={photo.id}
+      {/* Favbutton */}
+      <PhotoFavButton
+        photoId={photo.id} // prop move to child comp.
         toggleFav={toggleFav}
         favPhotos={favPhotos}
       />
-      <img className='photo-list__image'src={photo.urls.regular} onClick={handleOnClick}/>
+      {/* Photo card's view with data */}
+      <img className='photo-list__image' src={photo.urls.regular} onClick={handleOnClick} />
       <div className="photo-list__general-profile">
-        <img src={photo.user.profile}className='photo-list__user-profile'/>
+        <img src={photo.user.profile} className='photo-list__user-profile' />
         <div className="photo-list__details">
-          <p className="photo-list__user-info">{photo.user.name}</p> 
+          <p className="photo-list__user-info">{photo.user.name}</p>
           <p className="photo-list__user-location">{photo.location.city}-{photo.location.country}</p>
         </div>
       </div>

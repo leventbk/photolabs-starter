b69fbc468219b45343cb5a02bf211300968c400c
Leventbk <leventbkarakus@hotmail.com>
2023-09-29 16:23:37 -0400

    added comments

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -4,12 +4,14 @@ import "../styles/TopicListItem.scss";
 
 const TopicListItem = (props) => {
 
+  // function to handle click event for calling the function passed via props with the topicId
   const handleOnClick = () => {
     props.getPhotosbyTopic(props.topicId)
   }
   return (
     <div className="topic-list__item" onClick={handleOnClick}>
       <span>{props.label}</span>
+      {/* display topic label */}
     </div>
   );
 };

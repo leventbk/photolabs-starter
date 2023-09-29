86b5bb3fe77d04f86752ddc123533da4d345158f
Leventbk <leventbkarakus@hotmail.com>
2023-09-29 15:10:43 -0400

    comments added and document formatted

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -6,12 +6,13 @@ import '../styles/PhotoFavButton.scss';
 function PhotoFavButton(props) {
   const { toggleFav, photoId, favPhotos } = props;
 
-  const selected = favPhotos.includes(photoId); 
+  const selected = favPhotos.includes(photoId);
 
+  // selected prop variable added, onClick function added to trigger toggleFav by photoId
   return (
-    <div className="photo-list__fav-icon" onClick={()=> toggleFav(photoId)}>
+    <div className="photo-list__fav-icon" onClick={() => toggleFav(photoId)}>
       <div className="photo-list__fav-icon-svg">
-        <FavIcon selected={selected}/>
+        <FavIcon selected={selected} />
       </div>
     </div>
   );
